90b9a90d640b0b87576b3044bd2ec71d5cbbfeb3
Tateishi Tadatoshi <ishio39@gmail.com>
2021-11-18 15:24:32 +0900

    add lib.py, hello_message()

--- skeleton/__init__.py
+++ skeleton/__init__.py
@@ -1,5 +1,9 @@
 from . import cli
+from . import lib
 
 VERSION = '0.0.1'
 
+greeting = lib.greeting
+
 hello = cli.hello
+hello_message = cli.hello_message
--- skeleton/cli.py
+++ skeleton/cli.py
@@ -3,8 +3,24 @@ def hello() -> None:
 
     'Hello, World'と表示する
 
-    returns:
+    Returns:
         None
     """
 
     print("Hello, World")
+
+
+def hello_message() -> None:
+    """引数付きあいさつ
+
+    'Hello, {message}'と表示する
+
+    Args:
+        message(str): 表示するメッセージ
+
+    Returns:
+        None
+    """
+
+    from . import lib
+    print(f'Hello, {lib.greeting}')
