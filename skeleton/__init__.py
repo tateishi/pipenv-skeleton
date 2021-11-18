15dd92d53f7f7ed2cb805b87714faba9dbe49850
Tateishi Tadatoshi <ishio39@gmail.com>
2021-11-18 15:01:47 +0900

    First commit
    say Hello

---
+++ skeleton/__init__.py
@@ -0,0 +1,5 @@
+from . import cli
+
+VERSION = '0.0.1'
+
+hello = cli.hello
---
+++ skeleton/cli.py
@@ -0,0 +1,10 @@
+def hello() -> None:
+    """あいさつ
+
+    'Hello, World'と表示する
+
+    returns:
+        None
+    """
+
+    print("Hello, World")
